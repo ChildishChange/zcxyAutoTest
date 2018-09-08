9771e4675e579c5cb7ce531dc1e0ec21d112a5fe
ChildishChange <457694145@qq.com>
2018-09-09 00:15:15 +0800

    添加DownloadCode函数，可从URL直接下载代码文件

--- AutoTest/AutoTest/Program.cs
+++ AutoTest/AutoTest/Program.cs
@@ -71,5 +71,51 @@ namespace AutoTest
             return null;
         }
 
+        public bool DownloadCode(string Url)
+        {
+            string codePath = Directory.GetCurrentDirectory() + @"\temp";
+            Directory.CreateDirectory(codePath);
+            
+            
+            /*
+             需要添加一个解析json，读取下载信息的函数，来替换此处的test目录 
+             */
+            string codeFile = codePath + @"\" + "test";
+
+            if (File.Exists(codeFile))
+            {
+                Console.WriteLine("[INFO]Already downloaded!");
+                return true;
+            }
+            
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(Url) as HttpWebRequest;
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                
+                Stream responseStream = response.GetResponseStream();
+
+                FileStream fs = new FileStream(codeFile, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+
+                byte[] bArr = new byte[1024];
+                int size = responseStream.Read(bArr, 0, (int)bArr.Length);                
+                while (size > 0)
+                {
+                    fs.Write(bArr, 0, size);
+                    size = responseStream.Read(bArr, 0, (int)bArr.Length);
+                }
+
+                fs.Close();
+                responseStream.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            // https://github.com/ChildishChange/BlogPublishTool/archive/master.zip
+            return true;
+        }
     }
 }
