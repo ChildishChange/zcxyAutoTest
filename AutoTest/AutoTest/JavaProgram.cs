132073dd1478e1aa19190f66044aacdec20c1fe6
ChildishChange <457694145@qq.com>
2018-09-18 20:58:43 +0800

    修复JavaProgram的若干bug

--- AutoTest/AutoTest/JavaProgram.cs
+++ AutoTest/AutoTest/JavaProgram.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace AutoTest
 {
     public class JavaProgram
     {
         public string packageName { set; get; }
-        public string compileCmd { set; get; }
+        //public string compileCmd { set; get; }
         public string runTestCmd { set; get; }
         public string javaFilePath { set; get; }
         public bool canRunTest { set; get; }
@@ -28,28 +24,27 @@ namespace AutoTest
 
             CheckScannerAndPackage();
             if (!canRunTest) { return; }
+            
+            this.canRunTest = Tester.CallCmd($"javac {javaFilePath}") || 
+                              Tester.CallCmd($"javac -encoding UTF-8 {javaFilePath}");
 
-            if (GetEncoding() == Encoding.UTF8)
-            {
-                this.compileCmd = $"javac -encoding UTF-8 {javaFilePath}";
-            }
-            else
-            {
-                this.compileCmd = $"javac {javaFilePath}";
-            }
-
-            this.canRunTest = Tester.CallCmd(this.compileCmd);
             if (!canRunTest)
             {
                 Logger.Error($"Error happened when compiling {pspDir}");
                 return;
             }
-
-            //moveClassToPath
-            if(this.packageName!=null)
+            
+            if(!string.IsNullOrEmpty(this.packageName))
             {
+                if (Directory.Exists(Path.Combine(pspDir.FullName, this.packageName.Replace('.', '\\'))))
+                    Directory.Delete(Path.Combine(pspDir.FullName, this.packageName.Replace('.', '\\')),true);
                 Directory.CreateDirectory(Path.Combine(pspDir.FullName, this.packageName.Replace('.', '\\')));
-                this.runTestCmd = $"java -classpath {pspDir.FullName} {this.packageName}.{new FileInfo(this.javaFilePath).Name.Replace(".class", "")} ";
+                //移动class文件
+                File.Move(new FileInfo(this.javaFilePath).FullName.Replace(".java",".class"), 
+                          Path.Combine(pspDir.FullName, 
+                                       this.packageName.Replace('.', '\\'),
+                                       new FileInfo(this.javaFilePath).Name.Replace(".java", ".class")));
+                this.runTestCmd = $"java -classpath {pspDir.FullName} {this.packageName}.{new FileInfo(this.javaFilePath).Name.Replace(".java", "")} ";
             }
             else
             {
@@ -57,38 +52,7 @@ namespace AutoTest
             }
         }
 
-        /// <summary>
-        /// 获取java文件编码
-        /// code from https://www.cnblogs.com/guyun/p/4262587.html
-        /// </summary>
-        /// <returns></returns>
-        public Encoding GetEncoding()
-        {
-            //byte[] Unicode = new byte[] { 0xFF, 0xFE, 0x41 };
-            //byte[] UnicodeBIG = new byte[] { 0xFE, 0xFF, 0x00 };
-            //byte[] UTF8 = new byte[] { 0xEF, 0xBB, 0xBF }; //带BOM 
-            Encoding reVal = Encoding.Default;
-            FileStream fs = new FileStream(this.javaFilePath, FileMode.Open, FileAccess.Read);
-            BinaryReader r = new BinaryReader(fs, Encoding.Default);
-            int i;
-            int.TryParse(fs.Length.ToString(), out i);
-            byte[] ss = r.ReadBytes(i);
-            if (ss.Length > 3 && ss[0] == 0xEF && ss[1] == 0xBB && ss[2] == 0xBF)
-            {
-                reVal = Encoding.UTF8;
-            }
-            else if (ss.Length > 3 && ss[0] == 0xFE && ss[1] == 0xFF && ss[2] == 0x00)
-            {
-                reVal = Encoding.BigEndianUnicode;
-            }
-            else if (ss.Length > 3 && ss[0] == 0xFF && ss[1] == 0xFE && ss[2] == 0x41)
-            {
-                reVal = Encoding.Unicode;
-            }
-            r.Close();
-            fs.Close();
-            return reVal;
-        }
+
 
         /// <summary>
         /// 检查目录名与文件名是否符合PSP要求
@@ -133,11 +97,11 @@ namespace AutoTest
             //如果代码中含有package信息，则保存package信息
             var match = Regex.Match(_fileContent, @"(?:^|\n)(\s*)package(\s+)(.*)(\s*);");
             
-            this.packageName = match.Groups[0].Value.Trim();
+            //this.packageName = match.Groups[0].Value.Trim();
             this.packageName = match.Value.Replace("package","").Replace(";","").Trim();
             
             //如果代码中含有scanner，则设置
-            this.canRunTest = _fileContent.Contains("import java.util.Scanner;");
+            this.canRunTest = !_fileContent.Contains("import java.util.Scanner;");
             
         }
     }
