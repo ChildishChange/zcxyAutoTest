e38d4b2314c21885e20fb6611ee612b704b30a96
ChildishChange <457694145@qq.com>
2018-09-23 01:21:13 +0800

    修改了log的文件名

--- AutoTest/AutoTest/Logger.cs
+++ AutoTest/AutoTest/Logger.cs
@@ -6,7 +6,7 @@ namespace AutoTest
     //From https://github.com/SivilTaram/SudokuAutoTest/blob/86de8f44d063379445e59b22416c68bad337e3d7/SudokuAutoTest/Logger.cs\
     public static class Logger
     {
-        private static readonly string logPath = Path.Combine(Directory.GetCurrentDirectory(), "log");
+        private static readonly string logPath = Path.Combine(Directory.GetCurrentDirectory(), "log-"+ DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")+".txt");
 
         public static void Error(string message)
         {
